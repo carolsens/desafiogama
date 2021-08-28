a5492399a638d9504d24efa4721d84d06e2918d7
Carolina A. Sens <carolsens@gmail.com>
2021-08-28 18:26:06 -0300

    Adaptando porta da aplicacao backend ao Heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.listen('3000', () => {
+app.listen(process.env.PORT || '3000', () => {
     console.log('rodando na porta 3000')
 });
 
