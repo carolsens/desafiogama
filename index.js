d1ddf6029f0ac9ad2286f117c72361e085f1a54c
Carolina A. Sens <carolsens@gmail.com>
2021-08-28 18:02:33 -0300

    segunda atualizacao

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.listen('3000', () => {
+app.listen('443', () => {
     console.log('rodando na porta 3000')
 });
 
